8e44451affbc1de0d2c3d8174e50a95c2e71fc20
Alex Brazier <aejbrazier@gmail.com>
2014-11-27 12:30:45 +0000

    Added status route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,15 @@
 var ctrl = require('../controllers');
 var auth = require('../controllers/auth');
+var mongoose = require('mongoose');
 
 module.exports = function (app) {
+
+    app.get('/status', function(req, res){
+        if(mongoose.connection.readyState)
+            res.json({success: true});
+        else
+            res.status(500).json({error: true});
+    });
 
     app.post('/addmark', auth, ctrl.addMark);
 
